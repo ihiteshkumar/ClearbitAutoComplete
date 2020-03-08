3f757951966123ba76890f582cfae3acf49e71da
Hitesh Kumar <hiteshkumarhkp@gmail.com>
2020-03-08 23:00:42 +0530

    add autoComplete company search api

---
+++ ClearbitAutoComplete/Models/Company.swift
@@ -0,0 +1,15 @@
+//
+//  Company.swift
+//  ClearbitAutoComplete
+//
+//  Created by Hitesh Kumar on 08/03/20.
+//  Copyright © 2020 Hitesh Kumar. All rights reserved.
+//
+
+import Foundation
+
+struct Company: Codable {
+    let name: String
+    let domain: String
+    let logo: String
+}
--- ClearbitAutoComplete/Services/CompanySearch.swift
+++ ClearbitAutoComplete/Services/CompanySearch.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+private let searchRESTMethod = "/v1/companies/suggest"
+private let baseURL = "autocomplete.clearbit.com"
+
+protocol ComapanySearching: class {
+    func result(companies: [Company])
+    func fail(err: AppError)
+}
+
+class CompanySearch {
+    private lazy var service = WebService(baseURL: baseURL)
+    private weak var last: URLSessionTask?
+    public weak var delegate: ComapanySearching?
+    
+    func search(with query: String) {
+        if query.isEmpty {
+            delegate?.result(companies: [])
+            return
+        }
+        let queryDic = ["query": query]
+        let parse: (Data) -> [Company]? = { (data) -> [Company]? in
+            let jsdec = JSONDecoder()
+            var result: [Company]?
+            do {
+                result = try jsdec.decode([Company].self, from: data)
+            } catch let err {
+                print("not parsing", err)
+            }
+            return result
+        }
+        guard let allCompanies = try? service.prepareResource(pathForREST: searchRESTMethod, argsDict: queryDic, parse: parse) else {
+            delegate?.result(companies: [])
+            return
+        }
+        last = service.getMe(res: allCompanies) { [weak self] (result) in
+            switch result {
+            case let .success(events):
+                self?.delegate?.result(companies: events)
+            case let .failure(err):
+                self?.delegate?.fail(err: err)
+            }
+        }
+    }
+
+    func cancel() {
+        last?.cancel()
+    }
+}
